5a9b4f71b9e119557e5fd3cf28f7ef44b7f09dc4
mlayton20 <matthewlayton@Matthews-MBP.home>
2016-04-21 09:12:04 +0100

    Added click events to the cells. Blue changes position when clicked and stays when not clicked.

--- TapTheBlue/app/src/main/java/com/laytonlabs/android/taptheblue/GameActivity.java
+++ TapTheBlue/app/src/main/java/com/laytonlabs/android/taptheblue/GameActivity.java
@@ -1,12 +1,13 @@
 package com.laytonlabs.android.taptheblue;
 
 import android.app.Activity;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -15,36 +16,58 @@ import java.util.ArrayList;
  */
 public class GameActivity extends Activity {
 
+    private static final String TAG = "GameActivity";
     private ArrayList<Integer> occupiedCells;
     private RelativeLayout gameBoard;
+    private int currentBlueLocation = -1;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         gameBoard = (RelativeLayout)findViewById(R.id.game_board);
+        initBoard(gameBoard);
         clearBoard(gameBoard);
         placeBlueCell(gameBoard);
     }
 
     private void placeBlueCell(RelativeLayout gameBoard) {
+        currentBlueLocation = generateRandomNumber(gameBoard, -1);
         placeCell(gameBoard,
-                generateRandomNumber(gameBoard),
-                ContextCompat.getDrawable(this, R.drawable.round_button_selected));
+                currentBlueLocation,
+                R.drawable.round_button_selected);
     }
 
-    private void placeCell(RelativeLayout gameBoard, int i, Drawable drawable) {
-        //TODO - Add handler for this for API 15
-        ((Button)gameBoard.getChildAt(i)).setBackground(drawable);
+    private void placeBlueCell(RelativeLayout gameBoard, int prevId) {
+        currentBlueLocation = generateRandomNumber(gameBoard, prevId);
+        placeCell(gameBoard,
+                currentBlueLocation,
+                R.drawable.round_button_selected);
     }
 
-    private int generateRandomNumber(RelativeLayout gameBoard) {
+    private void placeCell(RelativeLayout gameBoard, int i, int drawable) {
+        Log.d(TAG, "Cell will be placed here: " + i);
+        if(android.os.Build.VERSION.SDK_INT < 16) {
+            ((Button)gameBoard.getChildAt(i)).setBackgroundResource(drawable);
+        } else {
+            ((Button)gameBoard.getChildAt(i)).setBackground(ContextCompat.getDrawable(this, drawable));
+        }
+    }
+
+    private int generateRandomNumber(RelativeLayout gameBoard, int prevId) {
         int selectedCell;
+        boolean isGoodNumber = false;
 
-        //TODO - Check to see if the below can select the first (0) and last cells (gameBoard.getChildCount()-1)
         do {
             selectedCell = (int)(Math.random() * gameBoard.getChildCount());
-        } while (!isFreeCell(selectedCell));
+            //Check to see if the cell is currently free.
+            if (isFreeCell(selectedCell)) {
+                //Now we want to check if the cell is not the same as it was before.
+                if (selectedCell != prevId) {
+                    isGoodNumber = true;
+                }
+            }
+        } while (!isGoodNumber);
 
         occupiedCells.add(selectedCell);
         return selectedCell;
@@ -59,17 +82,61 @@ public class GameActivity extends Activity {
         return true;
     }
 
+    private void initBoard(RelativeLayout gameBoard) {
+        for (int i = 0; i < gameBoard.getChildCount(); i++) {
+            View v = gameBoard.getChildAt(i);
+            Button button = (Button) v;
+
+            //Assign the non centre cells a value
+            if (button.getId() == -1) {
+                button.setId(i);
+            }
+
+            //Add a click listener for when a user presses the cell.
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Log.d(TAG, "Cell pressed: " + getButtonId(v.getId()));
+                    onCellTouch(getButtonId(v.getId()));
+                }
+            });
+        }
+    }
+
     private boolean clearBoard(RelativeLayout gameBoard) {
         occupiedCells = new ArrayList<>();
         for (int i = 0; i < gameBoard.getChildCount(); i++) {
             View v = gameBoard.getChildAt(i);
             if (v instanceof Button) {
-                //TODO - Add handler for this for API 15
-                ((Button)v).setBackground(ContextCompat.getDrawable(this, R.drawable.round_button));
+                Button button = (Button) v;
+
+                //Reset the background
+                if(android.os.Build.VERSION.SDK_INT < 16) {
+                    button.setBackgroundResource(R.drawable.round_button);
+                } else {
+                    button.setBackground(ContextCompat.getDrawable(this, R.drawable.round_button));
+                }
+
             } else {
                 return false;
             }
         }
         return true;
+    }
+
+    private void onCellTouch(int buttonId) {
+        //We only want to continue if the blue cell was selected. Otherwise it's game over.
+        if (buttonId != currentBlueLocation) {
+            //TODO - This would be game over.
+            Log.d(TAG, "Game Over!");
+            return;
+        }
+        clearBoard(gameBoard);
+        placeBlueCell(gameBoard, buttonId);
+    }
+
+    private int getButtonId(int id) {
+        //Since the centre button is allocated id in XML, we want to change that to be 0 for the game.
+        return id >= gameBoard.getChildCount() ? 0 : id;
     }
 }
